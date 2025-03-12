ad2201f00a0c253980c722de8346671a4f58f72f
Viet Huong <tranviethuong30102005@gmail.com>
2025-03-12 07:37:52 +0700

    test branch

---
+++ baitap1.cpp
@@ -0,0 +1,33 @@
+
+#include <stdio.h>
+
+#include <stdio.h>
+
+#include <stdio.h>
+
+#include <stdio.h>
+
+#include <stdio.h>
+#include <math.h>
+
+#include <stdio.h>
+
+int main() {
+    int x, y, z, t;
+    scanf("%d %d %d %d", &x, &y, &z, &t);
+    
+
+    printf("%d  %d  %d  %d\n", x, y, z, t);
+    
+   
+    printf("%d--%d--%d--%d\n", y, z, x, t);
+    
+   
+    printf("%d, %d, %d, %d\n", 2 * x, 3 * y, 4 * z, 5 * t);
+    
+  
+    printf("KET THUC !!\n");
+    
+    return 0;
+}
+
